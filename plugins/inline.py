9aaf7f35dd0fa72c0f7ab328c4e7891d1c0dea09
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-15 09:44:05 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -28,8 +28,8 @@ from config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-        InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
+        InlineKeyboardButton('⚡️ Support Channel', url='htpps://t.me/About_preeti'),
+        InlineKeyboardButton('🆙 Updates', url='https://t.me/Aboutvedmat'),
     ],
     [
         InlineKeyboardButton('🎧Play Music', url=f'https://t.me/{USERNAME}'),
