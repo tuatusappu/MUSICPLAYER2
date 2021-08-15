667d49cd73fd953c0d8bbaeb38590df1698d2131
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-15 10:12:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/MusicPlayer.git /MusicPlayer
-cd /MusicPlayer
+git clone https://github.com/subinps/MusicPlayer2.git /MusicPlayer2
+cd /MusicPlayer2
 echo "Starting Bot...."
 python3 main.py
